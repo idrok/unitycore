f9c77a23eb7719b2a25db105911d5405a7b54d9a
lengglxyz <11413947@qq.com>
2018-10-25 15:11:06 +0800

    UnityCore

---
+++ Assets/UnityCore/UnityCore_3D.cs
@@ -0,0 +1,279 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityCore_3D : MonoBehaviour {
+
+    //Vector3
+    private void Start_Vector3()
+    {
+        Vector3 a = new Vector3(0, 3, 0); //  3
+        Vector3 b = new Vector3(4, 0, 0); //  4
+        Vector3 c = new Vector3(4, 3, 0); //  c = a + b; 5
+
+        // Angle
+        /*
+        print( Vector3.Angle(b, c));//30 36.8699?
+        print(Vector3.Angle(a, c));//60 53.1301?
+        print(c.magnitude);//5 ok
+        */
+
+        // ClampMagnitude Vector3
+        // 限制一个向量大小，最大为maxLength，如果小于按照原向量返回
+        // 如果大于按照最大maxLength进行等比缩放
+        /*
+        print(Vector3.ClampMagnitude(c, 5.5f).magnitude); //5
+        print(Vector3.ClampMagnitude(c, 3f).magnitude); //3
+        */
+
+        // Cross Vector3
+        // 返回一个正对两个向量垂直的向量
+        // 这个方法会自动计算a向量和b向量的交叉点，返回垂直这两向量的垂直向量
+        /*
+        print(Vector3.Cross(a, b).normalized.ToString()); // -1
+        print(Vector3.Cross(a, c).normalized.ToString()); //-1
+        */
+
+        // Vector3.Distance(a,b) 计算向量a到向量b的距离
+
+        // a·b=|a|·|b|cos<a,b>
+        // 使用Dot来求角度的时候需要normalized，因为Acos取值在[-1,1]之间
+        /*
+        print(Mathf.Acos(Vector3.Dot(b.normalized, c.normalized)) * Mathf.Rad2Deg); // 30 36.8699?
+        print(Vector3.Dot(b, c));
+        */
+
+        // Vector3.kEpsilon ?
+
+        // 线性过渡a到b
+        // Vector3.Lerp(a, b, t);
+
+        // Vector3.LerpUnclamped = Lerp
+
+        // Vector3.Magnitude(v) 求一个向量的大小
+        /*
+        print(Vector3.Magnitude(a)); //3
+        print(Vector3.Magnitude(b)); //4
+        */
+
+        // Max 返回由ab中xyz组成最大值的新向量
+        // Min 返回由ab中xyz组成最小值的新向量
+        /*
+        print(Vector3.Max(a, b).ToString()); //(4,3,0)
+        print(Vector3.Min(a, c).ToString()); //(0,3,0)
+        */
+
+        // Vector3.MoveTowards(a, b, maxt)
+        // 类似于Leap，但这个可以限制maxt步进值
+        // maxt如果是正数向目标移动，maxt如果负数远离目标
+
+        // Vector3.negativeInfinity ?
+
+        // Vector3.Normalize(v)格式化一个向量值到[-1,1]
+
+        // OrthoNormalize(a,b,t)
+        // 这个一个组合方法，可以格式化a,b向量，并且找一个垂直于ab向量的t向量,相当于corss
+        /*
+        Vector3 t = Vector3.zero;
+        Vector3.OrthoNormalize(ref a, ref b, ref t);
+        print(a); // (0,1,0)
+        print(t.normalized.ToString());//(0,0,-1)
+        */
+
+        // Vector3.positiveInfinity ？
+
+        // Vector3.Project(v, p) 求一个向量在另一个向量上面的投影值
+        // print(Vector3.Project(c, b).ToString()); // (4,0,0)
+
+        // Vetor3.ProjectOnPlane(v, n)
+        // 法线一般是相对于平面来定义
+        // 切线一般是相对于曲面来定义
+        // 求一个向量在另一个向量法线上的投影值
+        //print(Vector3.ProjectOnPlane(c,b).ToString()); // (0,3,0)
+
+        // Vector3.ReferenceEquals(o1, o2) ?
+
+        // Vector3.Reflect(v, dir)
+        // 沿着dir方向的法线生成一个反射向量
+        /*
+        print(Vector3.Reflect(a, Vector3.right).ToString()); // (0,3,0)
+        print(Vector3.Reflect(a, Vector3.up).ToString()); //(0,-3,0)
+        */
+
+        // Vector3.RotateTowards(v, t, maxRad, maxmag)
+        // 相对于一个中心点由a点旋转到b点
+        //Vector3.RotateTowards(a, b, 100, 100); ?
+
+        //Vector3.Scale(a,b)将a和b的xyz分别相乘
+        //print(Vector3.Scale(a, b));// (0,0,0)
+
+        // 同Angle，但这个是带符号的角度，由正负之分
+        // Unity3d使用的左手坐标系，z轴往里面的
+        // print(Vector3.SignedAngle(a, c, Vector3.back));//53
+
+        // 相对于中心在弧线上插值， t[0,1]
+        //Vector3.Slerp(a, b, t)
+
+        // Vector3.SlerpUnclamped(a,b,t)
+
+        // 平滑阻尼，一般用于平滑跟随相机
+        // Vector3.SmoothDamp(c, t, v, t, maxs, t)
+        
+    }
+
+    //Quaternion
+    private void Start_Quaternion()
+    {
+        Quaternion a = Quaternion.AngleAxis(30, Vector3.up);
+        Quaternion b = Quaternion.AngleAxis(60, Vector3.up);
+
+        // Quaternion.Angle(q1, q2)
+        // 求两个四元素之间的角度
+        // print(Quaternion.Angle(a, b)); // 30
+
+        // Quaternion.AngleAxis(a, ax)
+        // 创建一个以ax为轴心的旋转角度
+
+        // Quaternion.Dot(q1, q2) ?
+        // print(Mathf.Acos( Quaternion.Dot(a, b)) * Mathf.Rad2Deg); 30 15 ?
+
+        // 使用Vector3来创建一个旋转实体，zxy的顺序
+        // Quaternion.Euler(v3)
+
+        // Quaternion.FromToRotation(v1, v2) ??
+        //print(Quaternion.FromToRotation(Vector3.up, Vector3.forward).ToString());
+
+        // 跟父物体保持同一旋转
+        //Quaternion.identity
+
+        // Quaternion.Inverse(q)
+        //反转一个旋转
+        //print(Quaternion.Inverse(a));
+
+        // Quaternion.kEpsilon ?
+
+        // 线性插值，t[0,1]
+        // Quaternion.Lerp(q1,q2,t)
+
+        // 线性插值，t可以随意值
+        //Quaternion.LerpUnclamped(q1,q2,t)
+
+        // 朝v1旋转，指定世界的up轴
+        // Quaternion.LookRotation(v1, up)
+
+        //保持q2和q1拥有一样的旋转
+        // Quaternion.RotateTowards(q1,q2,t)
+
+        //球形插值 t[0,1]
+        //Quaternion.Slerp(q1,q2,t)
+
+        //球形插座 t无限制
+        //Quaternion.SlerpUnclamped(q1,q2,t)
+
+        //lerp slerp 线性插线和光滑线性插值
+    }
+    
+    //Transform
+    private void Start_Transform()
+    {
+        // 给当前transform广播一条消息，包括其子节点上是脚本全部可以接收到
+        //transform.BroadcastMessage(n,p,o)
+
+        // 子节点数量
+        //transform.childCount
+
+        // 对比标签
+        //t.CompareTag(t)
+
+        // 子节点解体
+        //transform.DetachChildren()
+
+        // 相对于世界中的旋转
+        // print(transform.eulerAngles);
+
+        // 通过n来查找一个子节点
+        // transform.Find(n)
+
+        //世界坐标中的z x y
+        //transform.forward transform.right transform.up
+
+        // 通过索引获取子节点
+        //transform.GetChild(i)
+
+        // 获取当前的迭代器，迭代子节点
+        //transform.GetEnumerator()
+
+        // 哈希值
+        //transform.GetHashCode();
+
+        // 实例化ID
+        //t.GetInstanceID();
+
+        //获取在同级上的顺序索引
+        //t.GetSiblingIndex();
+
+        //设置隐藏属性
+        //transform.gameObject.hideFlags = HideFlags.NotEditable;
+
+        //统计从根节点的所有节点数量
+        //print(transform.hierarchyCapacity);
+
+        //统计从根节点的所有节点数量
+        //print(transform.hierarchyCount);
+
+        //将t1按照指定的d方向转换到世界坐标系 Vector3.forward << = t1.forward
+        //t1.TransformDirection(d)
+        //print(transform.TransformDirection(Vector3.forward).ToString());
+
+        //print( transform.TransformPoint(transform.position));
+        //print(transform.position);
+
+        //将t1按照右移动2个单位转换到世界坐标
+        //Vector3 needPos = transform.TransformPoint(Vector3.right * 2);
+        //Instantiate(go, needPos, Quaternion.identity);
+
+        // ??
+        //Vector3 needPos = transform.TransformVector(Vector3.right * 2);
+        //Instantiate(go, needPos, Quaternion.identity);
+
+        // 位移
+        //t.Translate(v, ax)
+
+        //???
+        //t.InverseTransformDirection(v)
+        //t.InverseTransformPoint(v)
+        //t.InverseTransformVector(v)
+
+        // 是否是t的子节点
+        //transform.IsChildOf(t)
+
+        //本地角度
+        //transform.localEulerAngles()
+
+        //transform.localPosition transform.localRotation transform.localScale
+
+        //本地到世界矩阵转换
+        //t.localToWorldMatrix
+
+        // 朝着upx轴向旋转
+        //transform.LookAt(v1, upx);
+
+        //世界坐标缩放
+        //transform.lossyScale
+
+        //跟节点
+        //transform.root
+
+        //相对于ax轴向的空间转动
+        //transform.Rotate(ax,a,s)
+
+        //一个点、一个轴、一个角度定义旋转
+        //transform.RotateAround(p,a,n)
+
+        //transform.SetAsFirstSibling();
+        //transform.SetAsLastSibling();
+        //transform.SetSiblingIndex(i);
+        //transform.SetPositionAndRotation(p, q)
+        //t.SetParent(t)
+    }
+}
---
+++ Assets/UnityCore/UnityCore_Physical.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityCore_Physical : MonoBehaviour {
+    
+    //Rigidbody
+	private void Start_Rigidbody () {
+        // 由一个点向周围爆炸的物体拟真效果
+        //b.AddExplosionForce(f,v,r,u,m)
+
+        // 给刚体施加一个力量
+        //b.AddForce(v,m)
+        //body.AddForce(Vector3.forward, ForceMode.Acceleration)
+
+        // 给刚体的一个点作用力
+        //b.AddForceAtPosition(f,v,m)
+
+        // 给刚体施加一个相对作用力
+        //b.AddRelativeForce(f,m)
+
+        // 施加一个相对扭矩力
+        //b.AddRelativeTorque(f,m)
+
+        // 给刚体施加一个扭矩力
+        //b.AddTorque(t,m)
+
+        // 物体的角阻力
+        //b.angularDrag
+
+        // 角速度向量
+        //b.angularVelocity
+
+        // 相对物体自身坐标物体质量的中心点
+        //b.centerOfMass
+
+        // 相对于v到碰撞器最近的一个点
+        //b.ClosestPointOnBounds(v)
+
+        //刚体的探查模式
+        //b.collisionDetectionMode
+
+        // 刚体的自由度模拟控制
+        //b.constraints
+
+        // 碰撞检测是否启用
+        //b.detectCollisions
+
+        // 物体的阻力
+        //b.drag
+
+        // 固定旋转
+        //b.freezeRotation
+
+        // 刚体在世界坐标中一点的速度
+        //b.GetPointVelocity
+
+        // 刚体在世界坐标中一点的相对速度
+        //b.GetRelativePointVelocity
+
+        // 物体的惯性张量
+        //b.inertiaTensor
+
+        //旋转的惯性张量
+        //b.inertiaTensorRotation
+
+        // 插值线性效果
+        //b.interpolation
+
+        // 是否附带刚体效果
+        //b.isKinematic
+
+        // 刚体是否静止
+        //b.IsSleeping()
+
+        // 刚体的质量
+        //b.mass
+
+        // 最大角速度
+        //b.maxAngularVelocity
+
+        // 最大非穿透速度 ?
+        //b.maxDepenetrationVelocity
+
+        //b.MovePosition(v)
+
+        //b.MoveRotation(q)
+
+        //b.position
+
+        //b.ResetCenterOfMass();
+
+        // 重置惯性张量和旋转
+        //b.ResetInertiaTensor();
+
+        //b.rotation
+
+        //设置质量，随着碰撞器的尺寸而缩放
+        //b.SetDensity(f)
+
+        //设置刚体静止
+        //b.Sleep()
+
+        //刚体静止阈值
+        //b.sleepThreshold
+
+        //??
+        //body.solverIterations
+
+        //body.solverVelocityIterations
+
+        //单一方向预测碰撞
+        //b.SweepTest(v, h, r, q);
+
+        //全部方向扫描
+        //b.SweepTestAll(v,d,q)
+
+        //b.transform
+
+        //b.useGravity
+
+        // 刚体的速度
+        //b.velocity
+
+        //b.WakeUp()
+
+        //在世界中的质量中心的
+        //b.worldCenterOfMass()
+	}
+
+    //Physical 物理属性和辅助方法
+    private void Start()
+    {
+        //图层蒙版常量可选择所有图层
+        //Physics.AllLayers;
+
+        //是否自动模拟物体效果
+        //Physics.autoSimulation
+
+        //如果transform改变的时候是否把状态同步到物理效果
+        //Physics.autoSyncTransforms
+
+        //碰撞反弹值
+        //Physics.bounceThreshold
+
+        //方块投射单个预测检测
+        //Physics.BoxCast(v,v,d,h,v,d,m,q)
+
+        //方块投射预测检测所有
+        //Physics.BoxCastAll(v,v,d,v,d,m,q)
+
+        //方块投身预测检测，buff结果
+        //Physics.BoxCastNonAlloc(v,v,d,hs,v,d,m,q)
+
+        //Physics.CapsuleCast(v,v,r,d,h,d,m,q)
+        //Physics.CapsuleCastAll(...)
+        //Physics.CapsuleCastNonAlloc(...)
+
+        //给定的盒子是否有重叠
+        //Physics.CheckBox(v,h,q,m,q)
+        //Physics.CheckCapsule(v,v2,r,m,q) 胶让体
+        //Physics.CheckSphere(v,r,m,q) 圆球
+
+        //指定位置上最接近碰撞器的点
+        //Physics.ClosestPoint(v,c,v,r)
+
+        // 检测两个碰撞器是否重叠
+        //Physics.ComputePenetration(c1,v1,q1,c2,v2,q2,d,d)
+        
+        // 新建collider默认偏移量
+        //Physics.defaultContactOffset
+
+        // 默认射线检测层级
+        //Physics.DefaultRaycastLayers
+
+        // 关节和碰撞器的准确程度
+        //Physics.defaultSolverIterations
+        //Physics.defaultSolverVelocityIterations
+
+        // 判定二个层级碰撞信息
+        //Physics.GetIgnoreLayerCollision(l1,l2)
+
+        //Physics.gravity
+
+        //让c1和c2忽略碰撞
+        //Physics.IgnoreCollision(c1,c2,i)
+        //Physics.IgnoreLayerCollision(l1,l2,i)
+        //Physics.IgnoreRaycastLayer
+
+        // 点到点投射,检测碰撞
+        //Physics.Linecast(v1,v2,h,m,q)
+        
+        // 碰撞实体检测
+        //Physics.OverlapBox(v,h,d,m,q)
+        //Physics.OverlapBoxNonAlloc(...)
+        //Physics.OverlapCapsule(...)
+        //Physics.OverlapCapsuleNonAlloc(...)  
+        //Physics.OverlapSphere(...)
+        //Physics.OverlapSphereNonAlloc(...)
+
+        // 是否击中后背 ?
+        //Physics.queriesHitBackfaces
+
+        // 是否触发
+        //Physics.queriesHitTriggers
+
+        // 射线检测
+        //Physics.Raycast(v,d,h,d,m,q)
+        //Physics.RaycastAll(...)
+        //Physics.RaycastNonAlloc(...)
+
+        // 临时模拟物理效果
+        //Physics.Simulate(f)
+
+        //球体碰撞预测
+        //Physics.SphereCast(v,r,d,h,v,m,q)
+        //Physics.SphereCastAll(...)
+        //Physics.SphereCastNonAlloc(...)
+
+        //Physics.SyncTransforms()
+    }
+}
